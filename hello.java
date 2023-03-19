80f98f39b449716ce1d1ef13be72eab60a2bc3c4
mahmudulhasanmilhan <mahmudulhasanmilhan@gmail.com>
2023-03-20 03:34:55 +0600

    we added some function for testing

--- hello.java
+++ hello.java
@@ -2,4 +2,21 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
+
+    public class Average {
+    public static void main(String[] args) {
+        int[] numbers = {5, 10, 15, 20, 25};
+        double average = calculateAverage(numbers);
+        System.out.println("The average is " + average);
+    }
+    
+    public static double calculateAverage(int[] numbers) {
+        double sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+        return sum / numbers.length;
+    }
+}
+
 }
