3b46fd12b59d15df68a1f2f53151da059c2a2fe0
mahmudulhasanmilhan <mahmudulhasanmilhan@gmail.com>
2023-04-13 19:21:42 +0600

    create two branch & change occur in one

--- hello.java
+++ hello.java
@@ -10,6 +10,13 @@ public class HelloWorld {
         System.out.println("The average is " + average);
     }
     
+    public static double calculateAverage(int[] numbers) {
+        double sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+        return sum / numbers.length;
+    }
 }
 
 }
